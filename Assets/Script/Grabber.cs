4b6090f21bbcd334083e43e22ff35c19a5834fe1
杨东雄 <2545493686@qq.com>
2019-05-07 21:32:44 +0800

    注释

--- Assets/Script/Grabber.cs
+++ Assets/Script/Grabber.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
 
-public class Grabber : MonoBehaviour {
+public class Grabber : MonoBehaviour
+{
     public SteamVR_Action_Boolean triggerAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("GrabPinch");
 
     bool hadGrabed = false;
@@ -13,13 +14,14 @@ public class Grabber : MonoBehaviour {
     Hand holdHand;
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
 
-        if (hadGrabed)
+        if (hadGrabed) //如果手上有东西
         {
-            if (triggerAction.GetStateUp(holdHand.handType))
+            if (triggerAction.GetStateUp(holdHand.handType)) //是否松开扳机
             {
-                grabTarget.transform.parent = grabOldParent;
+                grabTarget.transform.parent = grabOldParent; //复位
                 grabTarget.collider.enabled = true;
                 grabTarget.rigidbody.useGravity = true;
                 hadGrabed = false;
@@ -30,29 +32,29 @@ public class Grabber : MonoBehaviour {
 
         foreach (Hand hand in Player.instance.hands)
         {
-            if (triggerAction.GetStateDown(hand.handType))
+            if (triggerAction.GetStateDown(hand.handType)) //如果该手按下了扳机
             {
-                RaycastHit rayInfo = GameManager.Instance.pointers.CreateRaycast(30);
+                RaycastHit rayInfo = GameManager.Instance.pointers.CreateRaycast(30); //发送射线
 
-                if (rayInfo.collider.tag == "Throwable")
+                if (rayInfo.collider.tag == "Throwable") //如果射线目标是"Throwable"标签的物体
                 {
-                    if (!hadGrabed)
+                    if (!hadGrabed) //手上没有东西
                     {
-                        Grab(rayInfo);
+                        Grab(rayInfo); //抓起东西
                         holdHand = hand;
                     }
                 }
             }
         }
-	}
+    }
 
     private void Grab(RaycastHit ragInfo)
     {
         grabTarget = ragInfo;
         grabOldParent = ragInfo.transform.parent;
-        ragInfo.transform.parent = GameManager.Instance.GetHand().transform;
-        ragInfo.collider.enabled = false;
-        ragInfo.transform.GetComponent<Rigidbody>().useGravity = false;
+        ragInfo.transform.parent = GameManager.Instance.GetHand().transform; //设置目标物体的父坐标为手
+        ragInfo.collider.enabled = false;  //接触碰撞体
+        ragInfo.transform.GetComponent<Rigidbody>().useGravity = false; //解除重力
         hadGrabed = true;
     }
 }
--- Assets/Script/VRInputModule.cs
+++ Assets/Script/VRInputModule.cs
@@ -21,32 +21,30 @@ public class VRInputModule : BaseInputModule
     protected override void Awake()
     {
         base.Awake();
-        Data = new PointerEventData(eventSystem);
+        Data = new PointerEventData(eventSystem); //新建指针事件数据
     }
 
 
     public override void Process()
     {
+        Data.Reset(); //复位
+        Data.position = new Vector2(camera.pixelWidth / 2, camera.pixelHeight / 2); //设置指针坐标数据
 
+        eventSystem.RaycastAll(Data, m_RaycastResultCache); //根据指针数据发射射线，获取被照射到的物体
+        Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache); //设置指针下的当前物体为第一个被照射的物体
 
-        Data.Reset();
-        Data.position = new Vector2(camera.pixelWidth / 2, camera.pixelHeight / 2);
+        m_CurrentObject = Data.pointerCurrentRaycast.gameObject; //设置当前物体
 
-        eventSystem.RaycastAll(Data, m_RaycastResultCache);
-        Data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        m_RaycastResultCache.Clear(); //清除缓存
 
-        m_CurrentObject = Data.pointerCurrentRaycast.gameObject;
+        HandlePointerExitAndEnter(Data, m_CurrentObject); //发送鼠标进出事件
 
-        m_RaycastResultCache.Clear();
-
-        HandlePointerExitAndEnter(Data, m_CurrentObject);
-
-        if (clickAction.GetStateDown(targetSources))
+        if (clickAction.GetStateDown(targetSources)) //获取按键按下
         {
-            ProcessRelease(Data);
+            ProcessRelease(Data); 
         }
 
-        if (clickAction.GetStateUp(targetSources))
+        if (clickAction.GetStateUp(targetSources)) //获取按键松开
         {
             ProcessPress(Data);
         }
@@ -54,29 +52,33 @@ public class VRInputModule : BaseInputModule
 
     private void ProcessRelease(PointerEventData data)
     {
-        data.pointerPressRaycast = data.pointerCurrentRaycast;
+        data.pointerPressRaycast = data.pointerCurrentRaycast; //设置按下目标
+        //递归当前目标下适合的子对象，发送按下的消息，并返回对象
         GameObject newPointerPress = ExecuteEvents.ExecuteHierarchy(m_CurrentObject, data, ExecuteEvents.pointerDownHandler);
         if (newPointerPress == null)
-            newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject);
+            newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject); //获取能实现指针按下的子对象
 
-        data.pressPosition = data.position;
-        data.pointerPress = newPointerPress;
-        data.rawPointerPress = m_CurrentObject;
+        //Position of the press.
+        data.pressPosition = data.position; //设置按下坐标
+        //The GameObject that received the OnPointerDown.
+        data.pointerPress = newPointerPress; //设置收到按下信息的对象
+        //The object that the press happened on even if it can not handle the press event.
+        data.rawPointerPress = m_CurrentObject; //按下发生时的对象
     }
 
     private void ProcessPress(PointerEventData data)
     {
-        ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerUpHandler);
-        GameObject pointerClickHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject);
+        ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerUpHandler); //发送给物体松开的消息
+        GameObject pointerClickHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(m_CurrentObject); //获取能实现指针按下的子对象
 
-        if (data.pointerPress == pointerClickHandler)
+        if (data.pointerPress == pointerClickHandler) //如果和按下时的目标物体一样，发送按下消息
         {
             ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerClickHandler);
         }
 
-        eventSystem.SetSelectedGameObject(null);
+        eventSystem.SetSelectedGameObject(null); //移除焦点物体
 
-        data.pressPosition = Vector2.zero;
+        data.pressPosition = Vector2.zero; //复位
         data.pointerPress = null;
         data.rawPointerPress = null;
     }
